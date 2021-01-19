ef99b458676c608132bcc4215e266997b47bfb77
Kim Paolo Laberinto <git@kimlaberinto.com>
2021-01-19 17:29:29 -0600

    Add auth and getting tweet ids

--- deletetweets.jl
+++ deletetweets.jl
@@ -10,4 +10,12 @@ if !ispath(SECRET_KEYS_FILENAME)
 else
     secrets = YAML.load_file(SECRET_KEYS_FILENAME)
 
+    twitterauth(secrets["consumer_key"],
+        secrets["consumer_token"],
+        secrets["oauth_token"],
+        secrets["oauth_secret"])
+
+    user_timeline_of_tweets = get_user_timeline(screen_name = "KimPLab", count=500)
+    tweet_id = user_timeline_of_tweets[1].id
+    #post_destroy_single_tweet_id(string(tweet_id))
 end
