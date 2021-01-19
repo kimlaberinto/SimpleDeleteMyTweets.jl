6cf778e7024dfb23f657fe241f3cd7b5ae8f5cbc
Kim Paolo Laberinto <git@kimlaberinto.com>
2021-01-19 16:15:32 -0600

    Add minor error handling if file not found

--- deletetweets.jl
+++ deletetweets.jl
@@ -1,6 +1,13 @@
 import YAML
 using Twitter
+using Printf
 
 SECRET_KEYS_FILENAME = "secret_keys.yaml"
 
-secrets = YAML.load_file(SECRET_KEYS_FILENAME)
+if !ispath(SECRET_KEYS_FILENAME)
+    print(@sprintf "%s file not found! Please make sure to create the appropriate file containing your TWitter Developer keys." SECRET_KEYS_FILENAME)
+
+else
+    secrets = YAML.load_file(SECRET_KEYS_FILENAME)
+
+end
