9175ddc2aa582e3411ac2811ef95b1e9579be40e
Kim Paolo Laberinto <git@kimlaberinto.com>
2021-01-23 21:42:49 -0600

    Add simple Y/N confirmation for deletion

--- deletetweets.jl
+++ deletetweets.jl
@@ -5,7 +5,7 @@ using Printf
 SECRET_KEYS_FILENAME = "secret_keys.yaml"
 
 if !ispath(SECRET_KEYS_FILENAME)
-    print(@sprintf "%s file not found! Please make sure to create the appropriate file containing your TWitter Developer keys." SECRET_KEYS_FILENAME)
+    println(@sprintf "%s file not found! Please make sure to create the appropriate file containing your TWitter Developer keys." SECRET_KEYS_FILENAME)
 
 else
     secrets = YAML.load_file(SECRET_KEYS_FILENAME)
@@ -16,9 +16,20 @@ else
         secrets["oauth_secret"])
 
     user_timeline_of_tweets = get_user_timeline(screen_name = secrets["twitter_handle"], count=500)
-    for tweet in user_timeline_of_tweets
-        tweet_id = tweet.id
-        println(tweet_id)
-        #post_destroy_single_tweet_id(string(tweet_id))
+    number_tweets_for_deletion = length(user_timeline_of_tweets)
+
+    println(@sprintf "%d tweets found from account %s... Would you like to delete these tweets? (Y/N)" number_tweets_for_deletion secrets["twitter_handle"])
+    response = readline()
+    
+    if (response == "Y")
+        println(@sprintf """ '%s' response detected. Tweets will now be deleted...""" response)
+        for tweet in user_timeline_of_tweets
+            tweet_id = tweet.id
+            println(tweet_id)
+            #post_destroy_single_tweet_id(string(tweet_id))
+        end
+        println("The tweets have now been deleted. Exiting...")
+    else
+        println(@sprintf """ '%s' response detected. No tweets will be deleted. Exiting...""" response)
     end
 end
